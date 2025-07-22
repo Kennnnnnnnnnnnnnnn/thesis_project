e0547307b64a9f347c5d1aac7487d9aac536480c
vuth sakmakmol <vuthsakmakmol123@gmail.com>
2025-07-22 16:02:41 +0700

    make https hosting

--- src/api/config.js
+++ src/api/config.js
@@ -1,9 +1,9 @@
+// Use your domain in production
+const apiURL = "https://khmerrice.it.com";
+
+// Localhost for local testing (commented out)
+// const apiURL = "http://localhost:4000";
 // const apiURL = "http://10.0.2.246:4000";
-
-const apiURL = "http://localhost:4000";
-
 // const apiURL = "http://192.168.100.36:4000";
-
-
 
 export default apiURL;
--- src/services/socket.js
+++ src/services/socket.js
@@ -1,14 +1,29 @@
+// import { io } from 'socket.io-client';
+
+// // Automatically use backend port (adjust if deployed)
+// const socket = io(
+//   'http://localhost:4000',
+//   // 'http://157.245.59.122:4000',
+//    {
+//   transports: ['websocket'],
+//   withCredentials: true,
+//   autoConnect: true,
+//   reconnectionAttempts: 5
+// });
+
+// export default socket;
+
 import { io } from 'socket.io-client';
 
-// Automatically use backend port (adjust if deployed)
 const socket = io(
-  'http://localhost:4000',
-  // 'http://157.245.59.122:4000',
-   {
-  transports: ['websocket'],
-  withCredentials: true,
-  autoConnect: true,
-  reconnectionAttempts: 5
-});
+  'wss://khmerrice.it.com',
+  {
+    transports: ['websocket'],
+    withCredentials: true,
+    autoConnect: true,
+    reconnectionAttempts: 5
+  }
+);
 
 export default socket;
+
